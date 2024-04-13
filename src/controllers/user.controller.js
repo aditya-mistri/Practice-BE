5d0d30dff93ae522c1e07a8b156bbe95b35ea040
aditya <aditya@mait>
2024-04-13 17:48:24 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,97 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req,res)=>{
-  res.status(200).json({
-    message:"ok"
-  })  
+  /* 
+  
+  1. Get user details from frontend 
+  2. validation (if not empty etc..)
+  3. check if user already exist : username
+  4. check for images , check for avatar
+  5. upload them to cloudinary, avatar
+  6. create user object - create entry in db
+  7. remove password and refresh token field from response 
+  8. check for user creation 
+  9. return res (response)
+  
+  */
+   
+  // req.body => from -> json or form 
+  const { fullName , email , username , password  } 
+  = req.body
+
+/* 
+  if(fullName === ""){
+    throw new ApiError(400,"FullName is required")
+  } 
+*/
+
+if(
+    [fullName,email,username,password].some((field) => field?.trim()==="")
+){
+    throw new ApiError(400,"All fields are required")
+}
+
+const existedUser = User.findOne({
+    $or:[{ username },{ email }]
+})
+
+if(existedUser){
+    throw new ApiError(409,"User with email or username already exists")
+}
+//name = avatar so ,
+const avatarLocalPath = req.files?.avatar[0]?.path; 
+const coverImageLocalPath = req.files?.coverImage[0]?.path; 
+
+//check for avatar 
+if(!avatarLocalPath){
+    throw new ApiError(400,"Avatar file is required")
+}
+
+
+const avatar = await uploadOnCloudinary(avatarLocalPath) //because it takes time
+
+const coverImage = await uploadOnCloudinary(coverImageLocalPath) //because it takes time
+
+//check for avatar again
+if(!avatarLocalPath){
+  throw new ApiError(400,"Avatar file is required")
+}
+
+const user = await User.create({
+  
+  fullName,
+  avatar : avatar.url,
+  coverImage:coverImage?.url || "",
+  email,
+  password,
+  username:username.toLowerCase()
+
+})
+
+// to check user created or not , and delete password later
+
+const createdUser = await  User.findById(user._id).select(
+  "-password -refreshToken"
+)
+
+if(!createdUser){
+  throw new ApiError(500,"Server Error user not regist. ")
+}
+
+//now send response back
+//return it 
+
+return res.status(201).json(
+  new ApiResponse(200,createdUser,"User registered Successfully")
+)
+
+
+
+
 })
 
 export {registerUser} 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 
+
 const userSchema = new Schema({
     username:{
         type:String,
@@ -53,7 +54,7 @@ const userSchema = new Schema({
 userSchema.pre("save",async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 
